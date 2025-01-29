84e2dfdac8a5913a926d107e2cd170612f6818b2
sumakokima2 <44194648+sumakokima2@users.noreply.github.com>
2025-01-29 13:36:58 +0900

    Update commonkokima.js

--- commonkokima.js
+++ commonkokima.js
@@ -1,7 +1,6 @@
       // タブ切り替えの処理
       function switchTab(contentId, event) {
         id = contentId;
-        $("#editDialog").hide()
         if (contentId != "chart-container") {
             populateTable(contentId)
         } else {
@@ -39,3 +38,88 @@
         dropdown: true,
         scrollbar: true
     });
+
+    const getAccessObj = (data) => {
+        return {
+            url: API_URL,
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify(data),
+            timeout: 30000,
+        }
+    }
+
+    function loadData(idToken,type) {
+        $(".loader").show();
+        $.ajax(
+            getAccessObj({
+                path: type,
+                method: "get",
+                idToken: idToken
+            })
+        ).done(function (response) {
+            $(".loader").hide();
+            if (response.statusCode === 401) {
+                liff.logout();
+                window.location.reload();
+            } else if (response.statusCode !== 200) {
+                console.error(response.message);
+                alert(response.message)
+            } else {
+
+                allData = response.data.items
+                updateChart(response.data.items); // チャートとテーブルを更新
+
+                return response.data.items
+            }
+        }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Network error!");
+        });
+    }
+
+    function changeLanguage(lang) {
+        const knownLang = ["ja", "en"];
+        lang = knownLang.includes(lang) ? lang : "en";
+
+        $.getJSON("lang.json", (data) => {
+            $.each(data, function (index, val) {
+                const elm = $("." + val.class);
+                if (elm.length) {
+                    if (val.type === "docs") {
+                        elm.html(val[lang]);
+                    }
+                }
+            });
+        });
+    }
+
+    function addData(data,type) {
+//function setWeight(data) {
+    $(".loader").show();
+    $.ajax(
+        getAccessObj({
+            path: type,
+            method: "post",
+            idToken: token,
+            postData:data
+        })
+    ).done(function (response) {
+        if (response.statusCode === 401) {
+            liff.logout();
+            window.location.reload();
+        } else if (response.statusCode !== 200) {
+            console.error(response.message);
+            alert(response.message)
+        } else {
+            allData.push(data)
+
+            updateChart(allData); // グラフを更新
+            populateTable("table-container"); // テーブルを更新
+            populateTable("edit-container");
+            $(".loader").hide();
+        }
+    }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+        $(".loader").hide();
+        alert("Network error!");
+    });
+}
