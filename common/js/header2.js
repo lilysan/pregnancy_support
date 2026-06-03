a2218c10d56d4de6d166a453e89091ffa14ecb2f
Yurina <yurina.tkt@gmail.com>
2026-06-03 20:51:48 +0900

    スタッフ用ヘッダーの修正

--- common/js/header2.js
+++ common/js/header2.js
@@ -56,7 +56,8 @@ $(function () {
             type: "group",
             children: [
                 {label: "翻訳アプリ", path: "translation"},
-                {label: "スタッフ専用", path: "data"}
+                {label: "病院情報＜スタッフ専用＞", path: "hospital"},
+                {label: "患者データ＜スタッフ専用＞", path: "data"},
             ]
         }
     ];
