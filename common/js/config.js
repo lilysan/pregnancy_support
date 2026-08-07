246e5d13df14318976e2f5f02441b4fb25fa7678
sumakokima2 <sumakokima@gmail.com>
2026-08-08 03:16:54 +0900

    APIURL更新

--- common/js/config.js
+++ common/js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwsZ1qmuIBLBAaHBEhUiHd0Qji9Shavfrr2HMZuBGRDO6bA1rnIu0xo_fGI5nygGJ2p/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw8-aWwJLnFz1fdP9eTe9sTFc6QImdvj2zFw3ifS6-tcy4hMTBP9jfCZAdsUPK7wHKx/exec";
 const LIFF_ID = "2005594923-wN8OjyGd";
