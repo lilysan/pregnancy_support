0cdd8af185188bc0e5b0ff4fcd307f6bd916b5e6
Yurina <yurina.tkt@gmail.com>
2026-09-18 02:48:42 +0900

    user:getをCloud Runに移行テスト

--- common/js/config.js
+++ common/js/config.js
@@ -1,2 +1,3 @@
 const API_URL = "https://pregnancy-support-api.yurina-tkt.workers.dev/";
+const API_URL_V2 = "https://pregnancy-support-api-git-723335787644.asia-northeast1.run.app";
 const LIFF_ID = "2005594923-wN8OjyGd";
