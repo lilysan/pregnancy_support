681c7ae8ecc9503196e1ce948d47fb2492eb65ce
Yurina <yurina.tkt@gmail.com>
2026-09-17 21:58:07 +0900

    APIに中継サーバー追加

--- common/js/config.js
+++ common/js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw8-aWwJLnFz1fdP9eTe9sTFc6QImdvj2zFw3ifS6-tcy4hMTBP9jfCZAdsUPK7wHKx/exec";
+const API_URL = "https://pregnancy-support-api.yurina-tkt.workers.dev/";
 const LIFF_ID = "2005594923-wN8OjyGd";
